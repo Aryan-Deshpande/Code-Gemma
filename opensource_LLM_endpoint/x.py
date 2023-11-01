4f75143f399bb95392eea2d82bb25f6f93773a6a
Aryan Deshpande <aryan@Aryan.>
2023-11-02 02:02:31 +0530

    first

---
+++ opensource_LLM_endpoint/x.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+checkpoint = "bigcode/starcoder"
+device = "cpu" # for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+
+inputs = tokenizer.encode("def print_hello_world():", return_tensors="pt").to(device)
+outputs = model.generate(inputs)
+print(tokenizer.decode(outputs[0]))
